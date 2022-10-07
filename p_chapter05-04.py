076f087cf659cfe21256f1c4bb553f1dbe3bf0aa
ehghksvjscl <ehghkswodnjs@naver.com>
2022-10-07 09:30:02 +0900

    이터레이터, 제네레이터 공부중

--- p_chapter05-04.py
+++ p_chapter05-04.py
@@ -1,73 +1,75 @@
 """
 일급 함수
-클로저 심화
-왜부에서 호출된 함수의 변수값, 상태(레퍼러스) 복사 후 저장 -> 후에 접근(엑세스) 가능
+데코레이터(Decorator)
+
+장점 : 중복제거, 코드 간결, 
+       공통 함수 작성
+       로깅, 프레임워크, 유효성 체크 -> 공통 함수
+
+단점 : 가독성 감소?, 
+       특정 기능에 한정 된 함수는 -> 단일 함수가 유리
+       디버깅 불편
 """
 
-# Closure
-def closure_ex1():
-    # Free variable
-    # 클로저 영역
-    series = []
-
-    def averager(v):
-        series.append(v)
-        print("inner >>> {} / {}".format(series, len(series)))
-
-        return sum(series) / len(series)
-
-    return averager
+# 데코레이터 실습
+import time
 
 
-avg_closure1 = closure_ex1()
+def perf_clock(func):
+    def perf_clocked(*args):
+        # 함수 시작 시간
+        st = time.perf_counter()
 
-print(avg_closure1(10))
-print(avg_closure1(20))
-print(avg_closure1(30))
+        # 함수 실행
+        result = func(*args)
+
+        et = time.perf_counter() - st
+
+        # 실행 함수명
+        name = func.__name__
+
+        # 함수 메게 변수
+        arg_str = ", ".join(repr(arg) for arg in args)
+
+        # 결과 출력
+        print("[%0.5fs] %s(%s) -> %r" % (et, name, arg_str, result))
+
+        return result
+
+    return perf_clocked
+
+
+@perf_clock
+def time_func(seconds):
+    time.sleep(seconds)
+
+
+@perf_clock
+def sum_func(*numbers):
+    return sum(numbers)
+
+
+# 데코레이터 미사용
+non_deco1 = perf_clock(time_func)
+non_deco2 = perf_clock(sum_func)
+
+print(non_deco1, non_deco1.__code__.co_freevars)
+print(non_deco2, non_deco2.__code__.co_freevars)
+
+print("-" * 40, "Called None Decorator -> time_func")
+print()
+non_deco1(0.5)
+
+print("-" * 40, "Called None Decorator -> sum_func")
+print()
+non_deco2(100, 200, 300, 400, 500)
 
 print()
 print()
 
-# function inspection
-print(dir(avg_closure1))
-print(avg_closure1.__code__.co_freevars)
-print(avg_closure1.__closure__[0].cell_contents)
+# 데코레이터 사용
+print("-" * 40, "Called Decorator -> time_func")
+time_func(0.5)
 
-print()
-print()
-
-# 잘못된 클로저 사용의 예
-def closure_ex2():
-    # 자유 변수
-    cnt = 0
-    total = 0
-
-    def averager(v):
-        cnt += 1
-        total += v
-        return total / cnt
-
-    return averager
-
-
-avg_closure2 = closure_ex2()
-
-# print(avg_closure2(10))
-
-
-def closure_ex3():
-    # 자유 변수
-    cnt = 0
-    total = 0
-
-    def averager(v):
-        nonlocal cnt, total
-        cnt += 1
-        total += v
-        return total / cnt
-
-    return averager
-
-
-avg_closure3 = closure_ex3()
-print(avg_closure3(10))
+print("-" * 40, "Called None Decorator -> sum_func")
+sum_func(100, 200, 300, 400, 500)
---
+++ p_chapter06-01.py
@@ -0,0 +1,98 @@
+"""
+병행성(Concurrency)
+이터레이터, 제네레이터
+Iterator, Generator
+"""
+
+# 파이썬 반복 가능한 타입
+# collections, text file, List, Dict, Set, Tuple, unpacking, *args... : iterable)
+
+# 반복 가능한 이유? -> iter(x) 함수 호출
+from typing import Iterable
+
+
+t = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+print(dir(t))
+
+for c in t:
+    print(c, end="")
+
+
+print("")
+
+# while
+w = iter(t)
+
+while True:
+    try:
+        print(next(w), end="")
+    except StopIteration:
+        break
+
+print()
+print(hasattr(t, "__iter__"))
+print(isinstance(t, Iterable))  # t가 Iterable을 상속을 받았는지?
+
+print()
+print()
+
+# next 페턴
+class WordSplitter:
+    def __init__(self, text: str) -> None:
+        self._idx = 0
+        self._text = text.split(" ")
+
+    def __next__(self):
+        print("Called __next__")
+        try:
+            word = self._text[self._idx]
+        except IndexError:
+            raise StopIteration("Stopped Iteration")
+
+        self._idx += 1
+        return word
+
+    def __repr__(self) -> str:
+        return "WordSplit(%s)" % (self._text)
+
+
+w1 = WordSplitter("Do today what you could do tommorrow")
+print(next(w1))
+print(next(w1))
+print(next(w1))
+print(next(w1))
+print(next(w1))
+print(next(w1))
+print(next(w1))
+
+# Generator 패턴
+# 1. 지능형 리스트, 딕셔너리, 집합 -> 데이터 양 증가 후 메모리 사용양 증가
+#    그래서 제너레이터 사용 권장
+# 2. 단위 실행 가능한 코루틴(Corotine) 구현과 연동
+# 3. 작은 메모리 조각 사용
+
+
+class WordSplitGenator:
+    def __init__(self, text: str) -> None:
+        self._text = text.split(" ")
+
+    def __iter__(self):
+        for word in self._text:
+            yield word  # 제너리이터
+        return
+
+    def __repr__(self) -> str:
+        return "WordSplitGenerator(%s)" % (self._text)
+
+
+wg = WordSplitGenator("Do today what you could do tommorrow")
+wt = iter(wg)
+
+print(next(wt))
+print(next(wt))
+print(next(wt))
+print(next(wt))
+print(next(wt))
+print(next(wt))
+print(next(wt))
